6a4c2fb71fb1d4a96dc9c7060d775bacf31331f4
Chaima <1925046@alu.murciaeduca.es>
2024-10-09 12:13:40 +0200

    comprobaciones echas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,16 +10,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Integer> numeros = new ArrayList<>();
+        ArrayList<Integer> numeros = new ArrayList<>();//me creo la lista
 
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);//asigno la entrada a entrada
 
-        while (entrada.hasNextInt()) {
-            numeros.add(entrada.nextInt());
+
+        while (entrada.hasNextInt()) {//hasNextInt me comprueba que sea un entero
+            numeros.add(entrada.nextInt());//añado el numero a la lista
         }
-        Collections.sort(numeros);
+        Collections.sort(numeros);//ordena la lista
 
-        for (int i = 0; i < numeros.size(); i++) {
+        for (int i = 0; i < numeros.size(); i++) {//imprime la lista ordenada
             System.out.println(numeros.get(i));
         }
 
