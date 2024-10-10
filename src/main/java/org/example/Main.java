552e9214cb4941c8faaf46dd14315ef85c4b36ff
Chaima <1925046@alu.murciaeduca.es>
2024-10-10 08:42:40 +0200

    nombre cambiado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,8 @@
 package org.example;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -14,13 +11,13 @@ public class Main {
 
         Scanner entrada = new Scanner(System.in);//asigno la entrada a entrada
 
-
         while (entrada.hasNextInt()) {//hasNextInt me comprueba que sea un entero
             numeros.add(entrada.nextInt());//añado el numero a la lista
         }
         Collections.sort(numeros);//ordena la lista
 
-        for (int i = 0; i < numeros.size(); i++) {//imprime la lista ordenada
+        for(int i = 0; i < numeros.size(); i++)
+        {//imprime la lista ordenada
             System.out.println(numeros.get(i));
         }
 
